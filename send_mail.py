9843352a0baf6486275a177c0302a8c1eef0f738
Frederick Osunde <33795045+Frdrcksnd@users.noreply.github.com>
2019-08-26 08:28:57 -0700

    Add files via upload

---
+++ send_mail.py
@@ -0,0 +1,42 @@
+import imapclient
+import pprint
+import imaplib
+imaplib._MAXLINE = 10000000
+
+user_name =input("Enter your email address and press enter: ")
+password = input("Enter your password and press enter: ")
+server = imapclient.IMAPClient('imap.gmail.com', ssl=True)
+
+
+def main():
+    try:
+        server.login(user_name, password)
+    except Exception as exc:
+        print(f"There was an error: {exc}")
+    else:
+        print(f"Successfully logged in to {user_name}")
+
+    try:
+        server.select_folder('INBOX', readonly=False)
+        UIDs = server.search(['UNSEEN'])
+
+        print(len(UIDs))
+
+    except Exception as exc:
+        print(f"There was an error: {exc}")
+
+    try:
+        for mssg_ID in UIDs:
+            raw_message = server.fetch(mssg_ID, ['BODY[]'])
+            pprint.pprint(raw_message)
+
+    except Exception as exc:
+        print(f"There was an error: {exc}")
+
+    else:
+        print("Done!!!")
+
+        server.logout()
+
+if  __name__ == "__main__" :
+    main()
