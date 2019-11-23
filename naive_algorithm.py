70da887946cfff607389ddebbe476ad3160e36d3
Frederick Osunde <33795045+Frdrcksnd@users.noreply.github.com>
2019-11-23 13:07:21 -0800

    Naive_algorithms

--- naive_algorithm.py
+++ naive_algorithm.py
@@ -1,5 +1,9 @@
-def naive_algorithm(n, num):
+from random import *
+
+
+def naive_algorithm( num):
     product = 0
+    n = len(num)
     for i in range(n):
         for j in range(n):
             if i != j:
@@ -20,7 +24,7 @@ def opt_naive_algorithm(n, num):
 
 def fast_naive_algorithm(n):
     index1 = 0
-    for i in range(1, len(n)):
+    for i in range(len(n)):
         if n[i] > n[index1]:
             index1 = i
     if index1 == 0:
@@ -28,10 +32,36 @@ def fast_naive_algorithm(n):
     else:
         index2 = 0
 
-    for j in range(1, len(n)):
-        if n[j] > n[index2] and j != i:
+    for j in range( len(n)):
+        if n[j] > n[index2] and j != index1:
             index2 = j
+    print(index1, index2)
 
     return n[index1] * n[index2]
 
-print(fast_naive_algorithm([2,1]))
+
+def stress_test(x, y):
+    ok = True
+    while ok:
+        n = randint(2, x)
+        array = []
+
+        for i in range(n):
+            array.append(randint(0, y))
+        print(array[0:])
+
+        result_1 = naive_algorithm(array)
+        result_2 = fast_naive_algorithm(array)
+
+        if result_1 == result_2:
+            print('OK')
+        else:
+            print("Wrong answer", result_1, result_2)
+            ok = False
+    return
+
+
+
+
+if __name__ == "__main__":
+    print(stress_test(2, 1))
